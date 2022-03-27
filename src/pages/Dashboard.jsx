bb909096492740443e230390f6b72250e75deb35
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-27 09:46:05 +0600

    design dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <ul>
+        <li>Name :</li>
+        <li>Email :</li>
+        <li>Logout :</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Dashboard;
