96864ce0c0615d2747ef9e6c5f305e49f213ec0c
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-27 13:31:59 +0600

    solve error

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { authLogout } from "../redux/reducers/LoginSlice";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.login);
   const handleLogout = () => {
     localStorage.clear();
     dispatch(authLogout());
   };
+
   return (
     <div>
       <ul>
-        <li>Name :</li>
-        <li>Email :</li>
-        <li onClick={handleLogout}>Logout :</li>
+        <li>Name :{user && user.name}</li>
+        <li>Email :{user && user.email}</li>
+        <li onClick={handleLogout}>Logout</li>
       </ul>
     </div>
   );
